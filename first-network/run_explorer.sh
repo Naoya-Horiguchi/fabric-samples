7117bd1d95cd3285e9471c1355bb14a5aafe4bf4
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2019-06-17 13:36:40 +0900

    説明を追加
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- first-network/run_explorer.sh
+++ first-network/run_explorer.sh
@@ -1,3 +1,14 @@
+#
+#  手順:
+#
+#    1. 先に byfn.sh で一通り Fabric ネットワークを開始する
+#       
+#       ./byfn.sh generate -c mychannel
+#       ./byfn.sh up -f docker-compose-cli.yaml -c mychannel [-s couchdb]
+#
+#    2. このスクリプトを走らせて追加で explorer, explorerdb を起動する
+#    3. localhost:8090 にブラウザでアクセスする
+
 privatekey=$(find crypto-config/ | grep peerOrganizations/org1.example.com/users/Admin@org1.example.com | grep _sk | xargs -r basename)
 
 if [ ! "$privatekey" ] ; then
